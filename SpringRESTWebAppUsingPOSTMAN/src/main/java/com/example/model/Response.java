753226d2294adf1923a4ce8aca239d9562143d19
user <user@DESKTOP-2K37FSJ.bbrouter>
2022-06-24 23:06:18 +0530

    third commit

---
+++ SpringRESTWebAppUsingPOSTMAN/src/main/java/com/example/model/Response.java
@@ -0,0 +1,28 @@
+package com.example.model;
+
+public class Response
+{
+	private String status;
+	private int code;
+	private String data;
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+
+}
---
+++ SpringRESTWebAppUsingPOSTMAN/src/main/java/com/example/springstarter/AnimalController.java
@@ -0,0 +1,13 @@
+package com.example.springstarter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class AnimalController {
+	@RequestMapping("jenny")
+	public String home1()
+	{
+		System.out.println("Capgemini");
+		return "animal.txt";
+	}
+}
